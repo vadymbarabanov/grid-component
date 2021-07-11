424003d5d2f26ffa71450148221c9a04d490b643
Vadim Barabanov <introvert.png@gmail.com>
2021-07-11 20:05:51 +0300

    Responsive props and alignItems & justifyContent

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,28 @@ import GridRuler from "./components/GridRuler/GridRuler";
 
 function App() {
   return (
-    <div style={{ margin: "16px", position: "relative" }}>
+    <div style={{ margin: "16px", position: "relative", height: "100vh" }}>
       <GridRuler gap="sm" />
-      <Grid container gap="sm">
-        <Grid item cols={4}>
+      <Grid
+        container
+        gap="sm"
+        justifyContent="center"
+        alignItems="center"
+        style={{ height: "100%" }}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box 1</Box>
         </Grid>
 
-        <Grid item cols={4}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box 2</Box>
         </Grid>
 
-        <Grid item cols={4}>
+        <Grid item xs={1} sm={6} md={4} lg={3}>
           <Box>Box 3</Box>
+        </Grid>
+
+        <Grid item xs={1} sm={6} md={4} lg={3}>
+          <Box>Box 4</Box>
         </Grid>
       </Grid>
     </div>
--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -4,30 +4,57 @@ import cn from "classnames";
 
 type ColsType = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 type GapType = "sm" | "md" | "lg";
+type JustifyContentType =
+  | "center"
+  | "space-around"
+  | "space-between"
+  | "flex-start"
+  | "flex-end";
+type AlignItemsType = "center" | "flex-start" | "flex-end";
 
 type GridProps = {
   children: ReactNode;
   container?: boolean;
   item?: boolean;
-  cols?: ColsType;
+  xs?: ColsType;
+  sm?: ColsType;
+  md?: ColsType;
+  lg?: ColsType;
   gap?: GapType;
-};
+  justifyContent?: JustifyContentType;
+  alignItems?: AlignItemsType;
+} & React.HTMLAttributes<HTMLDivElement>;
 
 const Grid: FC<GridProps> = ({
   children,
   container = false,
   item = false,
-  cols,
+  xs,
+  sm,
+  md,
+  lg,
   gap,
+  justifyContent,
+  alignItems,
+  ...rest
 }) => {
   const classNames = cn({
     [styles.Grid_container]: container,
     [styles.Grid_item]: item,
-    [styles[`Grid_cols_${cols}`]]: cols ? true : false,
-    [styles[`Grid_gap_${gap}`]]: gap ? true : false,
+    [styles[`Grid_xs_${xs}`]]: xs,
+    [styles[`Grid_sm_${sm}`]]: sm,
+    [styles[`Grid_md_${md}`]]: md,
+    [styles[`Grid_lg_${lg}`]]: lg,
+    [styles[`Grid_gap_${gap}`]]: gap,
+    [styles[`Grid_justifyContent_${justifyContent}`]]: justifyContent,
+    [styles[`Grid_alignItems_${alignItems}`]]: alignItems,
   });
 
-  return <div className={classNames}>{children}</div>;
+  return (
+    <div className={classNames} {...rest}>
+      {children}
+    </div>
+  );
 };
 
 export default Grid;
